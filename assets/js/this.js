1c66ff3c590e0f8872b8eae0426b5f2631160107
Pavel <80585863+Svygzhryr@users.noreply.github.com>
2021-11-19 09:13:10 +0300

    Add files via upload

--- assets/js/this.js
+++ assets/js/this.js
@@ -8,7 +8,9 @@ const progress  = document.querySelector('.progress');
 const info      = document.querySelector('.player_text');
 const volumeCont= document.querySelector('.volume_container');
 const getVolume = document.querySelector('.volume');
-// получаю треки
+const pThumb    = document.querySelector('.progress_thumb');
+const loopBtn   = document.querySelector('.nav-loop');
+// получаю песни
 const track     = document.querySelector('#track');
 
 
@@ -78,22 +80,51 @@ function nextSong() {
 
 }
 
+function loopTrack() {
+
+    if (loopBtn.classList.contains('looped')) {
+        loopBtn.classList.remove('looped');
+        document.querySelector('.fa-sync-alt').style.color = 'inherit';
+    } else {
+        loopBtn.classList.add('looped');
+        document.querySelector('.fa-sync-alt').style.color = '#131313';
+    }
+    
+}
+
 // шаманим с длительностью аудиофайла
 function updateProgress(e) {
     const {duration, currentTime} = e.srcElement
     const progressPercent = (currentTime / duration) * 100;
+
 
     progress.style.width = `${progressPercent}%`
 
     if (progressPercent > 1) { 
         progress.style.boxShadow = "0px 0px 10px 3.5px rgba(19, 19, 19, 0.82)";
     } else {
-        progress.style.boxShadow = 'none';
+        progress.style.boxShadow = 'none';       
+    }
+
+    if (progressPercent > 3) { 
+        pThumb.style.opacity = "1";
+    } else {
+        pThumb.style.opacity = '0';       
     }
 
     if (progressPercent >= 100) {
-        nextSong();
+        
+        if (loopBtn.classList.contains('looped')) {
+
+            track.currentTime = 0;
+
+            playSong();
+        }   else {
+            nextSong();
+        }
+
     }
+
 
 }
 
@@ -125,9 +156,21 @@ playBtn.addEventListener('click', () => {
 
 });
 
+track.addEventListener("timeupdate", function() {
+    var s = parseInt(track.currentTime % 60);
+    var m = parseInt((track.currentTime / 60) % 60);
+    if (s <= 9) {
+        document.querySelector('.nav-time').innerHTML = m + ':0' + s ;  
+    } else {
+        document.querySelector('.nav-time').innerHTML = m + ':' + s ;  
+    }
+}, false);
+
+
+
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 track.addEventListener('timeupdate', updateProgress);
 bar.addEventListener('click', setProgress);
 getVolume.addEventListener('change', setVolume);
-
+loopBtn.addEventListener('click', loopTrack);

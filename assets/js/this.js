cb26673721a0c21a670dede032ee0c4b0d8bbe4a
Pavel <80585863+Svygzhryr@users.noreply.github.com>
2021-11-27 14:31:37 +0300

    Add files via upload

--- assets/js/this.js
+++ assets/js/this.js
@@ -20,7 +20,7 @@ console.log(activeImg, prevBtn, startBtn, stopBtn, nextBtn, bar, info); */
 
 // массив песенок
 
-const songs = ['Redbone - DontCry', 'Jockel der Gartenteichspringbrunnen', 'Imagine Dragons - Enemy', 'Bones - Sesh', 'UnKnown - BigTown']
+const songs = ['Redbone - DontCry', 'RHODAMINE - House of the Addict', 'Imagine Dragons - Enemy', 'Bones - Sesh', 'UnKnown - BigTown']
 
 let songIndex = 0;
 
@@ -103,19 +103,17 @@ function updateProgress(e) {
     if (progressPercent > 1) { 
         progress.style.boxShadow = "0px 0px 10px 3.5px rgba(19, 19, 19, 0.82)";
     } else {
-        progress.style.boxShadow = 'none';       
+        progress.style.boxShadow = 'none';
     }
 
     if (progressPercent > 3) { 
         pThumb.style.opacity = "1";
     } else {
-        pThumb.style.opacity = '0';       
+        pThumb.style.opacity = '0';
     }
 
-    if (progressPercent >= 100) {
+    if (progressPercent >= 100 && loopBtn.classList.contains('looped')) {
         
-        if (loopBtn.classList.contains('looped')) {
-
             track.currentTime = 0;
 
             playSong();
